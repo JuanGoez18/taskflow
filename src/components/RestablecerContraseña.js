c05f5165fa82e1f27f1c4076ef23c36ac9853adc
JuanGoez18 <160356853+JuanGoez18@users.noreply.github.com>
2025-06-19 08:33:43 -0500

    Update RestablecerContraseña.js

--- src/components/RestablecerContraseña.js
+++ src/components/RestablecerContraseña.js
@@ -40,7 +40,7 @@ if (errores.length > 0) {
 }
 
 try {
-    const res = await fetch("http://localhost:5000/restablecer", {
+    const res = await fetch("https://taskflownodesvr.onrender.com/restablecer", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ token, nuevaContraseña: nueva }),
