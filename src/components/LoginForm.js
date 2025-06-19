e493ffb985a577c665776190a8b58188ed018aeb
JuanGoez18 <160356853+JuanGoez18@users.noreply.github.com>
2025-06-18 19:51:35 -0500

    Update LoginForm.js

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -33,7 +33,7 @@ useEffect(() => {
   }
 
   try {
-    const response = await fetch("http://localhost:5000/login", {
+    const response = await fetch("https://taskflownodesvr.onrender.com/login", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ email, contraseña: password }),
